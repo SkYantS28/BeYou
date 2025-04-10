669b7e295ba75db97d3b9745567506c80755a050
Gabriel Teixeira <gabrieltf04@gmail.com>
2025-04-10 16:30:11 -0300

    Pesquisar em processo

--- src/Pesquisar.js
+++ src/Pesquisar.js
@@ -1,76 +1,3 @@
-/*import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import {
-    Background,
-    Header,
-    LogoContainer,
-    ImgLogo,
-    Tres,
-    Perfil,
-    ImgPerfil,
-    PerfilLink,
-    BotaoPerfil,
-    BarraPesquisa,
-    TitleCategorias,
-    Categorias,
-    CategoriasImg,
-}from "./components/pesquisarestrutura";
-import { Title } from "./components/favoritosestilos";
-
-const SearchPage = () => {
-  const [menuVisible, setMenuVisible] = useState(false);
-
-  const navigate = useNavigate(); 
-    // abrir e fechar menu
-    const toggleMenu = () => {
-        setMenuVisible(!menuVisible);
-      };
-    
-        // recarregar pagina
-        const reloadPage = (event) => {
-          event.preventDefault();
-          window.location.reload();
-        };
-
-  return (
-    <Background>
-      <Header>
-        <LogoContainer>
-          <ImgLogo onClick={reloadPage} src="/img/new_logo.jpg" alt="Logo" />
-        </LogoContainer>
-        
-        <Tres onClick={toggleMenu}>
-          &#9776;
-        </Tres>
-        
-        <Perfil>
-          <PerfilLink onClick={() => navigate("/MeuPerfil")}>
-            <ImgPerfil src="/img/perfil.eu.jpg" />
-          </PerfilLink>
-        </Perfil>
-        
-        <BotaoPerfil onClick={() => navigate("/MeuPerfil")}>Seu perfil</BotaoPerfil>
-
-      </Header>
-
-      <BarraPesquisa type="text" placeholder="Pesquisar" />
-
-      <TitleCategorias>Categorias</TitleCategorias>
-
-      <Categorias>
-        <CategoriasImg src="/img/categoria_sobrancelha.jpg" alt="Sobrancelhas"/>
-        <CategoriasImg src="/img/categoria_unhas.avif" alt=""/>
-        <CategoriasImg src="/img/categoria_coloracao.webp" alt=""/>
-        <CategoriasImg src="/img/categoria_trancas.webp" alt=""/>
-      </Categorias>
-
-    </Background>
-  );
-};
-
-export default SearchPage;*/
-//--------------------------------------------------------------
-
 import React, { useState } from "react";
 import {
     Background,
@@ -85,25 +12,20 @@ import {
     BarraPesquisa,
     TitleCategorias,
     Categorias,
-    CategoriasImg,
-    Footer,
-    ConteudoFooter,
-    Coluna,
-    SocialIcons,
-    FooterLink,
-    Direitos,
-    TitleFooter,
-    Texto
+    CategoriaContainer,
+    TextoSobreposto,
+    CategoriasImg
 } from "./components/pesquisarestrutura";
 
 const SearchPage = () => {
     const [searchTerm, setSearchTerm] = useState("");
 
     const categories = [
-        { name: "Sobrancelhas", img: "/img/categoria_sobrancelha.jpg" },
+        { name: "Sobrancelhas", img: "/img/categoria_sobrancelha.jpg"},
         { name: "Unhas", img: "/img/categoria_unhas.avif" },
         { name: "Coloração", img: "/img/categoria_coloracao.webp" },
         { name: "Tranças", img: "/img/categoria_trancas.webp" },
+        { name: "Extensão de cílios", img: "/img/categoria_cilios.jpg" },
     ];
 
     // Filtrar categorias com base no termo de pesquisa
@@ -139,50 +61,17 @@ const SearchPage = () => {
             <TitleCategorias>Categorias</TitleCategorias>
 
             <Categorias>
-                {filteredCategories.length > 0 ? (
-                    filteredCategories.map((category, index) => (
-                        <CategoriasImg key={index} src={category.img} alt={category.name} />
-                    ))
-                ) : (
-                    <p style={{ textAlign: 'center', width: '100%' }}>Nenhuma categoria encontrada.</p>
+            {filteredCategories.length > 0 ? (
+                filteredCategories.map((category, index) => (
+                    <CategoriaContainer key={index}>
+                        <CategoriasImg src={category.img} alt={category.name} />
+                        <TextoSobreposto>{category.name}</TextoSobreposto>
+                    </CategoriaContainer>
+                ))
+            ) : (
+            <p style={{ textAlign: 'center', width: '100%' }}>Nenhuma categoria encontrada.</p>
                 )}
             </Categorias>
-
-            <Footer>
-                <ConteudoFooter>
-                    <Coluna>
-                        <TitleFooter>BeYou</TitleFooter>
-                        <FooterLink>Site Institucional</FooterLink>
-                        <FooterLink>Fale Conosco</FooterLink>
-                        <FooterLink>Conta e Segurança</FooterLink>
-                        <FooterLink>Carreiras</FooterLink>
-                        <FooterLink>Entregadores</FooterLink>
-                    </Coluna>
-
-                    <Coluna>
-                        <TitleFooter>Descubra</TitleFooter>
-                        <FooterLink>Cadastre sua loja</FooterLink>
-                        <FooterLink>Beyou Shop</FooterLink>
-                        <FooterLink>BeYou Empresas</FooterLink>
-                        <FooterLink>Blog BeYou Empresas</FooterLink>
-                    </Coluna>
-
-                    <Coluna>
-                        <TitleFooter>Social</TitleFooter>
-                        <SocialIcons>
-                            <FooterLink>Facebook</FooterLink>
-                            <FooterLink>Twitter</FooterLink>
-                            <FooterLink>YouTube</FooterLink>
-                            <FooterLink>Instagram</FooterLink>
-                        </SocialIcons>
-                    </Coluna>
-
-                    <Direitos>
-                        <Texto>©️ Copyright 2025 - BeYou. Todos os direitos reservados.</Texto>
-                        <Texto>CNPJ 00.000.000/0000-00 / Endereço fictício - Cidade/Estado - CEP 00000-000</Texto>
-                    </Direitos>
-                </ConteudoFooter>
-            </Footer>
         </Background>
     );
 };
--- src/components/pesquisarestrutura.js
+++ src/components/pesquisarestrutura.js
@@ -1,167 +1,3 @@
-/*import styled from "styled-components";
-
-const Background = styled.div`
-    background-color: #FFC9E9;
-    min-height: 100vh;
-`;
-
-const Header = styled.header`
-    background-image: url("/img/Textura_Rosa.jpg");
-    min-height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat; 
-    background-size: cover;
-    position: relative;
-    color: #6c4539;
-    padding: 20px;
-    display: flex; 
-    align-items: center;
-    justify-content: space-between;
-    border: 0.5px solid #6c4539;
-`;
-
-const LogoContainer = styled.div`
-    display: flex;
-    align-items: left;
-`;
-
-const ImgLogo = styled.img`
-    width: 80px;
-    height: auto;
-    border-radius: 50%;
-    margin-right: 800px;
-    align-items: center;
-    border: 2px solid #6c4539;
-    cursor: pointer;
-`;
-
-const Tres = styled.span`
-    color: #6c4539;
-    cursor: pointer;
-
-    &:focus {
-        .tres:hover{
-        color: #5E17EB;
-    }
-
-    &:hover {
-        color: #5E17EB;
-    }
-`;
-
-const Perfil = styled.div`
-    margin-right: 0;
-    display: flex;
-`;
-
-const ImgPerfil = styled.img`
-    width: 80px;
-    height: auto;
-    border-radius: 50%;
-    transition: filter 0.3s ease;
-    border: 2px solid #6c4539;
-    align-items: center;
-    margin-right: 20px;
-    cursor: pointer;
-
-    &:focus {
-        filter: grayscale(100%);
-        border-color: black;
-    }
-`;
-
-const PerfilLink = styled.a`
-  color: #f45bf4;
-  text-decoration: none;
-
-  &:hover {
-    text-decoration: underline;
-    color: hsl(300, 99%, 64%);
-  }
-`;
-
-const BotaoPerfil = styled.button`
-    box-sizing: border-box;
-    cursor: pointer;
-    padding: 15px;
-    text-align: center;
-    font-size: 15px;
-    font-weight: bold;
-    margin-left: 0px;
-    transition: color 0.3s ease;
-    list-style-type: none;
-    color: black;
-    background-color: #fd99d5;
-    border: solid 2px #6c4539;
-    margin-bottom: 2px;
-    opacity: 0.8;
-    font-size: 20px;
-    border-radius: 20px;
-
-  &:hover {
-    background-color: #FFC9E9;
-    border: solid 2px #6c4539;
-    color: white;
-    cursor: pointer;
-  }
-`;
-
-const BarraPesquisa = styled.input`
-  width: 370px;
-  padding: 12px;
-  margin: 15px;
-  border: solid 1px #6c4539;
-  background-color: #ffecfc;
-  border-radius: 20px;
-  cursor: pointer;
-  margin-left: 530px;
-`;
-
-const TitleCategorias = styled.h1`
-  margin-left: 50px;
-  margin-top: -55px;
-  font-size: 40px;
-`;
-
-const Categorias = styled.div`
-  margin-top: 50px;
-  overflow: auto;
-  white-space: nowrap;
-  scrollbar-width: none;
-`;
-
-const CategoriasImg = styled.img`
-    margin-bottom: 0px;
-    width: 350px;
-    height: 300px;
-    margin-left: 20px;
-    border-radius: 20%;
-    border: solid 2px #6c4539;
-  &:hover {
-    filter: brightness(0.8);
-    border: solid 2px #ff9a6e;
-    cursor: pointer;
-  }
-`;
-
-
-export {
-  Background,
-  Header,
-  LogoContainer,
-  ImgLogo,
-  Tres,
-  Perfil,
-  ImgPerfil,
-  PerfilLink,
-  BotaoPerfil,
-  BarraPesquisa,
-  TitleCategorias,
-  Categorias,
-  CategoriasImg
-};
-*/
-
 import styled from "styled-components";
 
 const Background = styled.div`
@@ -262,71 +98,43 @@ const TitleCategorias = styled.h1`
 `;
 
 const Categorias = styled.div`
-    margin-top: 50px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  gap: 80px;
+  overflow: hidden;
+`;
+
+const CategoriaContainer = styled.div`
+  position: relative;
+  width: 80%;
+  margin: 10px 0;
+`;
+
+const TextoSobreposto = styled.div`
+  position: absolute;
+  top: 80%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-shadow: 2px 2px 6px rgba(0, 0, 0, 0.8);
+  pointer-events: none;
+  text-align: center;
 `;
 
 const CategoriasImg = styled.img`
-    width: 350px;
+    margin-bottom: 20px;
+    width: 330px;
     height: 300px;
-    margin: 20px;
+    margin-left: 10px;
     border-radius: 20%;
     border: solid 2px #6c4539;
-
-    &:hover {
-        filter: brightness(0.8);
-        border: solid 2px #ff9a6e;
-        cursor: pointer;
-    }
-`;
-
-const Footer = styled.footer`
-    background-color: #fff;
-    padding: 20px;
-    margin-top: 20px;
-`;
-
-const ConteudoFooter = styled.div`
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-`;
-
-const Coluna = styled.div`
-    flex: 1;
-    min-width: 200px;
-    margin: 10px;
-`;
-
-const SocialIcons = styled.div`
-    display: flex;
-    gap: 10px;
-`;
-
-const FooterLink = styled.a`
-    color: #6c4539;
-    text-decoration: none;
-
-    &:hover {
-        text-decoration: underline;
-    }
-`;
-
-const Direitos = styled.div`
-    text-align: center;
-    margin-top: 20px;
-`;
-
-const TitleFooter = styled.h2`
-    font-size: 24px;
-    margin-bottom: 10px;
-`;
-
-const Texto = styled.p`
-    font-size: 14px;
-    color: #666;
+  &:hover {
+    filter: brightness(0.8);
+    border: solid 2px #ff9a6e;
+    cursor: pointer;
+  }
 `;
 
 export {
@@ -342,14 +150,8 @@ export {
     BarraPesquisa,
     TitleCategorias,
     Categorias,
-    CategoriasImg,
-    Footer,
-    ConteudoFooter,
-    Coluna,
-    SocialIcons,
-    FooterLink,
-    Direitos,
-    TitleFooter,
-    Texto
+    CategoriaContainer,
+    TextoSobreposto,
+    CategoriasImg
 };
 
