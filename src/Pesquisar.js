e6bcb1526722691145cd8dc311aad9e4960b530b
Gabriel Teixeira <gabrieltf04@gmail.com>
2025-04-10 02:25:19 -0300

    Atualização pesquisar

--- src/Pesquisar.js
+++ src/Pesquisar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
     Background,
@@ -66,6 +66,125 @@ const SearchPage = () => {
 
     </Background>
   );
+};
+
+export default SearchPage;*/
+//--------------------------------------------------------------
+
+import React, { useState } from "react";
+import {
+    Background,
+    Header,
+    LogoContainer,
+    ImgLogo,
+    Tres,
+    Perfil,
+    ImgPerfil,
+    PerfilLink,
+    BotaoPerfil,
+    BarraPesquisa,
+    TitleCategorias,
+    Categorias,
+    CategoriasImg,
+    Footer,
+    ConteudoFooter,
+    Coluna,
+    SocialIcons,
+    FooterLink,
+    Direitos,
+    TitleFooter,
+    Texto
+} from "./components/pesquisarestrutura";
+
+const SearchPage = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const categories = [
+        { name: "Sobrancelhas", img: "/img/categoria_sobrancelha.jpg" },
+        { name: "Unhas", img: "/img/categoria_unhas.avif" },
+        { name: "Coloração", img: "/img/categoria_coloracao.webp" },
+        { name: "Tranças", img: "/img/categoria_trancas.webp" },
+    ];
+
+    // Filtrar categorias com base no termo de pesquisa
+    const filteredCategories = categories.filter(category =>
+        category.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <Background>
+            <Header>
+                <LogoContainer>
+                    <ImgLogo src="/img/new_logo.jpg" alt="Logo" />
+                </LogoContainer>
+
+                <Tres>&#9776;</Tres>
+
+                <Perfil>
+                    <PerfilLink>
+                        <ImgPerfil src="/img/perfil.eu.jpg" />
+                    </PerfilLink>
+                </Perfil>
+
+                <BotaoPerfil>Seu perfil</BotaoPerfil>
+            </Header>
+
+            <BarraPesquisa
+                type="text"
+                placeholder="Pesquisar categorias"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+
+            <TitleCategorias>Categorias</TitleCategorias>
+
+            <Categorias>
+                {filteredCategories.length > 0 ? (
+                    filteredCategories.map((category, index) => (
+                        <CategoriasImg key={index} src={category.img} alt={category.name} />
+                    ))
+                ) : (
+                    <p style={{ textAlign: 'center', width: '100%' }}>Nenhuma categoria encontrada.</p>
+                )}
+            </Categorias>
+
+            <Footer>
+                <ConteudoFooter>
+                    <Coluna>
+                        <TitleFooter>BeYou</TitleFooter>
+                        <FooterLink>Site Institucional</FooterLink>
+                        <FooterLink>Fale Conosco</FooterLink>
+                        <FooterLink>Conta e Segurança</FooterLink>
+                        <FooterLink>Carreiras</FooterLink>
+                        <FooterLink>Entregadores</FooterLink>
+                    </Coluna>
+
+                    <Coluna>
+                        <TitleFooter>Descubra</TitleFooter>
+                        <FooterLink>Cadastre sua loja</FooterLink>
+                        <FooterLink>Beyou Shop</FooterLink>
+                        <FooterLink>BeYou Empresas</FooterLink>
+                        <FooterLink>Blog BeYou Empresas</FooterLink>
+                    </Coluna>
+
+                    <Coluna>
+                        <TitleFooter>Social</TitleFooter>
+                        <SocialIcons>
+                            <FooterLink>Facebook</FooterLink>
+                            <FooterLink>Twitter</FooterLink>
+                            <FooterLink>YouTube</FooterLink>
+                            <FooterLink>Instagram</FooterLink>
+                        </SocialIcons>
+                    </Coluna>
+
+                    <Direitos>
+                        <Texto>©️ Copyright 2025 - BeYou. Todos os direitos reservados.</Texto>
+                        <Texto>CNPJ 00.000.000/0000-00 / Endereço fictício - Cidade/Estado - CEP 00000-000</Texto>
+                    </Direitos>
+                </ConteudoFooter>
+            </Footer>
+        </Background>
+    );
 };
 
 export default SearchPage;
--- src/components/pesquisarestrutura.js
+++ src/components/pesquisarestrutura.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+/*import styled from "styled-components";
 
 const Background = styled.div`
     background-color: #FFC9E9;
@@ -159,5 +159,197 @@ export {
   TitleCategorias,
   Categorias,
   CategoriasImg
+};
+*/
+
+import styled from "styled-components";
+
+const Background = styled.div`
+    background-color: #FFC9E9;
+    min-height: 100vh;
+`;
+
+const Header = styled.header`
+    background-image: url("/img/Textura_Rosa.jpg");
+    min-height: 100%; 
+    background-position: center;
+    background-repeat: no-repeat; 
+    background-size: cover;
+    position: relative;
+    color: #6c4539;
+    padding: 20px;
+    display: flex; 
+    align-items: center;
+    justify-content: space-between;
+    border: 0.5px solid #6c4539;
+`;
+
+const LogoContainer = styled.div`
+    display: flex;
+    align-items: left;
+`;
+
+const ImgLogo = styled.img`
+    width: 80px;
+    height: auto;
+    border-radius: 50%;
+    margin-right: auto;
+    border: 2px solid #6c4539;
+    cursor: pointer;
+`;
+
+const Tres = styled.span`
+    color: #6c4539;
+    cursor: pointer;
+
+    &:hover {
+        color: #5E17EB;
+    }
+`;
+
+const Perfil = styled.div`
+    display: flex;
+`;
+
+const ImgPerfil = styled.img`
+    width: 80px;
+    height: auto;
+    border-radius: 50%;
+    border: 2px solid #6c4539;
+    margin-right: 20px;
+    cursor: pointer;
+`;
+
+const PerfilLink = styled.a`
+    color: #f45bf4;
+    text-decoration: none;
+
+    &:hover {
+        text-decoration: underline;
+        color: hsl(300, 99%, 64%);
+    }
+`;
+
+const BotaoPerfil = styled.button`
+    cursor: pointer;
+    padding: 15px;
+    font-size: 20px;
+    font-weight: bold;
+    color: black;
+    background-color: #fd99d5;
+    border: solid 2px #6c4539;
+    border-radius: 20px;
+
+    &:hover {
+        background-color: #FFC9E9;
+        color: white;
+    }
+`;
+
+const BarraPesquisa = styled.input`
+    width: 400px; /* Aumentado para maior visibilidade */
+    padding: 12px;
+    margin: 20px auto; /* Centralizado */
+    border: solid 1px #6c4539;
+    background-color: #ffecfc;
+    border-radius: 20px;
+    display: block; /* Para centralizar corretamente */
+`;
+
+const TitleCategorias = styled.h1`
+    margin-left: 50px;
+    font-size: 40px;
+`;
+
+const Categorias = styled.div`
+    margin-top: 50px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`;
+
+const CategoriasImg = styled.img`
+    width: 350px;
+    height: 300px;
+    margin: 20px;
+    border-radius: 20%;
+    border: solid 2px #6c4539;
+
+    &:hover {
+        filter: brightness(0.8);
+        border: solid 2px #ff9a6e;
+        cursor: pointer;
+    }
+`;
+
+const Footer = styled.footer`
+    background-color: #fff;
+    padding: 20px;
+    margin-top: 20px;
+`;
+
+const ConteudoFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+`;
+
+const Coluna = styled.div`
+    flex: 1;
+    min-width: 200px;
+    margin: 10px;
+`;
+
+const SocialIcons = styled.div`
+    display: flex;
+    gap: 10px;
+`;
+
+const FooterLink = styled.a`
+    color: #6c4539;
+    text-decoration: none;
+
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
+const Direitos = styled.div`
+    text-align: center;
+    margin-top: 20px;
+`;
+
+const TitleFooter = styled.h2`
+    font-size: 24px;
+    margin-bottom: 10px;
+`;
+
+const Texto = styled.p`
+    font-size: 14px;
+    color: #666;
+`;
+
+export {
+    Background,
+    Header,
+    LogoContainer,
+    ImgLogo,
+    Tres,
+    Perfil,
+    ImgPerfil,
+    PerfilLink,
+    BotaoPerfil,
+    BarraPesquisa,
+    TitleCategorias,
+    Categorias,
+    CategoriasImg,
+    Footer,
+    ConteudoFooter,
+    Coluna,
+    SocialIcons,
+    FooterLink,
+    Direitos,
+    TitleFooter,
+    Texto
 };
 
