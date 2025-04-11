abb833f63e6db5647d3c01c08114edcf223739bc
Sky Crizosti <166828481+SkYantS28@users.noreply.github.com>
2025-04-11 10:19:39 -0300

    Update Agendamentos.js

--- src/Agendamentos.js
+++ src/Agendamentos.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { User, Bell, LogOut } from "lucide-react";
 import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
-import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import {
     Background,
@@ -21,6 +20,8 @@ import {
     Icons,
 
     Title,
+    CustomCalendar,
+    Dot,
   
     Footer,
     ConteudoFooter,
@@ -31,16 +32,17 @@ import {
     Logo,
     Direitos,
     TitleFooter,
-    Texto
+    Texto,
+    GlobalStyle
 } from "./components/agendamentosestrutura";
 
-function Agendamentos() {
+function Agendamento() {
+    // linkar paginas
+    const navigate = useNavigate(); 
+
     const handleLogout = () => {
         navigate("/bemvindo"); 
       };
-  
-    // linkar paginas
-    const navigate = useNavigate(); 
   
     // levar ate a pagina inicial
     const reloadPage = (event) => {
@@ -48,7 +50,6 @@ function Agendamentos() {
         navigate('/PaginaInicial');
     };
 
-        
     const appointments = {
         "2025-04-05": [
         { store: "Loja A", service: "Corte de Cabelo", time: "10:00", price: "R$ 50,00" },
@@ -59,113 +60,128 @@ function Agendamentos() {
         ],
     };
 
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    
-      const formattedDate = selectedDate.toISOString().split("T")[0];
-      const dayAppointments = appointments[formattedDate] || [];
+    const [date, setDate] = useState(new Date());
+
+    const tileContent = ({ date, view }) => {
+        const dateString = date.toISOString().split("T")[0];
+        if (appointments[dateString]) {
+            return <Dot />
+        }
+        return null;
+    };
+
   
     return (
         <Background>
-            <Header>
-                <LogoContainer>
-                    <ImgLogo onClick={reloadPage} src="/img/new_logo.jpg" alt="Logo" />
-                </LogoContainer>
+            <GlobalStyle />
+                <Header>
+                    <LogoContainer>
+                        <ImgLogo onClick={reloadPage} src="/img/new_logo.jpg" alt="Logo" />
+                    </LogoContainer>
 
-                <Menu>
-                    <MenuLink onClick={() => navigate("/PaginaInicial")}>Inicio</MenuLink>
+                    <Menu>
+                        <MenuLink onClick={() => navigate("/PaginaInicial")}>Inicio</MenuLink>
 
-                    <MenuLink onClick={() => navigate("/Carrinho")}>Meu Carrinho</MenuLink>
+                        <MenuLink onClick={() => navigate("/Carrinho")}>Meu Carrinho</MenuLink>
 
-                    <MenuLink onClick={() => navigate("/Pesquisar")}>Pesquisar</MenuLink>
+                        <MenuLink onClick={() => navigate("/Pesquisar")}>Pesquisar</MenuLink>
 
-                    <MenuLink onClick={() => navigate("/Agendamentos")}>Agendamentos</MenuLink>
+                        <MenuLink onClick={() => navigate("/Agendamentos")}>Agendamentos</MenuLink>
 
-                    <MenuLink onClick={() => navigate("/FAvoritos_perfil")}>Favoritos</MenuLink>
+                        <MenuLink onClick={() => navigate("/FAvoritos_perfil")}>Favoritos</MenuLink>
 
-                </Menu>
+                    </Menu>
+                    
+                    <SearchBar type="text" placeholder="Busque por item ou loja" />
+
+                    <Icons>
+                        <Perfil>
+                            <PerfilLink onClick={() => navigate("/MeuPerfil")}>
+                                <User className="icone" />
+                            </PerfilLink>
+                        </Perfil>
                 
-                <SearchBar type="text" placeholder="Busque por item ou loja" />
+                        <Noticacao>
+                            <NoticacaoLink onClick={() => navigate("/notificacoes_perfil")}>
+                                <Bell className="icone" />
+                            </NoticacaoLink>
+                        </Noticacao>
+                
+                        <Sair>
+                            <BotaoSair onClick={handleLogout}>
+                                <LogOut className="icone" />
+                            </BotaoSair>
+                        </Sair>
+                    </Icons>
 
-                <Icons>
-                    <Perfil>
-                        <PerfilLink onClick={() => navigate("/MeuPerfil")}>
-                            <User className="icone" />
-                        </PerfilLink>
-                    </Perfil>
-            
-                    <Noticacao>
-                        <NoticacaoLink onClick={() => navigate("/notificacoes_perfil")}>
-                            <Bell className="icone" />
-                        </NoticacaoLink>
-                    </Noticacao>
-            
-                    <Sair>
-                        <BotaoSair onClick={handleLogout}>
-                            <LogOut className="icone" />
-                        </BotaoSair>
-                    </Sair>
-                </Icons>
+                </Header>
 
-            </Header>
+                <main>
+                    <Title>
+                        Agendamentos
+                    </Title>
 
-            <main>
-            </main> 
+                    <CustomCalendar
+                        onChange={setDate}
+                        value={date}
+                        tileContent={tileContent}
+                    />
+                </main> 
 
-            <Footer>
-                <ConteudoFooter>
-                    <Coluna>
-                        <TitleFooter>BeYou</TitleFooter>
+                <Footer>
+                    <ConteudoFooter>
+                        <Coluna>
+                            <TitleFooter>BeYou</TitleFooter>
 
-                        <FooterLink>Site Institucional</FooterLink>
-                        <FooterLink>Fale Conosco</FooterLink>
-                        <FooterLink>Conta e Segurança</FooterLink>
-                        <FooterLink>Carreiras</FooterLink>
-                        <FooterLink>Entregadores</FooterLink>
+                            <FooterLink>Site Institucional</FooterLink>
+                            <FooterLink>Fale Conosco</FooterLink>
+                            <FooterLink>Conta e Segurança</FooterLink>
+                            <FooterLink>Carreiras</FooterLink>
+                            <FooterLink>Entregadores</FooterLink>
 
-                    </Coluna>
+                        </Coluna>
 
-                    <Coluna>
-                        <TitleFooter>Descubra</TitleFooter>
+                        <Coluna>
+                            <TitleFooter>Descubra</TitleFooter>
 
-                        <FooterLink>Cadastre sua loja</FooterLink>
-                        <FooterLink>Beyou Shop</FooterLink>
-                        <FooterLink>BeYou Empresas</FooterLink>
-                        <FooterLink>Blog BeYou Empresas</FooterLink>
-                    </Coluna>
-                    
-                    <Coluna>
-                        <TitleFooter>Social</TitleFooter>
-                        <SocialIcons>
-                            <FooterLink><FaFacebookF/></FooterLink>
-                            <FooterLink><FaTwitter/></FooterLink>
-                            <FooterLink><FaYoutube/></FooterLink>
-                            <FooterLink><FaInstagram/></FooterLink>
-                        </SocialIcons>
-                    </Coluna>
-                    
-                    <Direitos>
-
-                        <Logo src="/img/new_logo.jpg" alt="BeYou Logo" />
+                            <FooterLink>Cadastre sua loja</FooterLink>
+                            <FooterLink>Beyou Shop</FooterLink>
+                            <FooterLink>BeYou Empresas</FooterLink>
+                            <FooterLink>Blog BeYou Empresas</FooterLink>
+                        </Coluna>
                         
-                        <Texto>© Copyright 2025 - BeYou. Todos os direitos reservados.</Texto>
-
-                        <br></br>
+                        <Coluna>
+                            <TitleFooter>Social</TitleFooter>
+                            <SocialIcons>
+                                <FooterLink><FaFacebookF/></FooterLink>
+                                <FooterLink><FaTwitter/></FooterLink>
+                                <FooterLink><FaYoutube/></FooterLink>
+                                <FooterLink><FaInstagram/></FooterLink>
+                            </SocialIcons>
+                        </Coluna>
                         
-                        <Texto>CNPJ 00.000.000/0000-00 / Endereço fictício - Cidade/Estado - CEP 00000-000</Texto>
+                        <Direitos>
 
-                    </Direitos>
+                            <Logo src="/img/new_logo.jpg" alt="BeYou Logo" />
+                            
+                            <Texto>© Copyright 2025 - BeYou. Todos os direitos reservados.</Texto>
 
-                    <Links>
-                        <FooterLink>Termos e Condições de Uso</FooterLink> | <FooterLink>Codigo de Conduta</FooterLink> | <FooterLink>Privacidade</FooterLink> | <FooterLink>Dicas de Segurança</FooterLink>
-                    </Links>
+                            <br></br>
+                            
+                            <Texto>CNPJ 00.000.000/0000-00 / Endereço fictício - Cidade/Estado - CEP 00000-000</Texto>
 
-                </ConteudoFooter>
+                        </Direitos>
 
-            </Footer>
+                        <Links>
+                            <FooterLink>Termos e Condições de Uso</FooterLink> | <FooterLink>Codigo de Conduta</FooterLink> | <FooterLink>Privacidade</FooterLink> | <FooterLink>Dicas de Segurança</FooterLink>
+                        </Links>
+
+                    </ConteudoFooter>
+
+                </Footer>
 
         </Background>
     );
 }
 
-
-export default Agendamentos();
+export default Agendamento();
