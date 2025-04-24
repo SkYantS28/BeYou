d75a1c95f9c19e327dfd00d03c6adbc30c121c1a
Sky Crizosti <166828481+SkYantS28@users.noreply.github.com>
2025-04-23 23:26:23 -0300

    Update dadosperfilestrutura.js

--- src/components/dadosperfilestrutura.js
+++ src/components/dadosperfilestrutura.js
@@ -1,53 +1,53 @@
 import styled from "styled-components";
 
 const Background = styled.div`
-  background-color: rgb(255, 228, 230);
+    background-color:rgb(255, 228, 230);
 `;
 
 const Header = styled.header`
-  background-image: url("/img/Textura_Rosa.jpg");
-  min-height: 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  position: relative;
-  color: #6c4539;
-  padding: 10px 20px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  border: 0.5px solid #6c4539;
+    background-image: url("/img/Textura_Rosa.jpg");
+    min-height: 100%; 
+    background-position: center;
+    background-repeat: no-repeat; 
+    background-size: cover;
+    position: relative;
+    color: #6c4539;
+    padding: 10px 20px;
+    display: flex; 
+    align-items: center;
+    justify-content: space-between;
+    border: 0.5px solid #6c4539;
 `;
 
 const LogoContainer = styled.div`
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 `;
 
 const ImgLogo = styled.img`
-  width: 60px;
-  height: auto;
-  border-radius: 50%;
-  border: 2px solid #6c4539;
-  cursor: pointer;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    width: 60px;
+    height: auto;
+    border-radius: 50%;
+    border: 2px solid #6c4539;
+    cursor: pointer;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 `;
 
 const SearchBar = styled.input`
-  width: 30%;
-  padding: 10px;
-  border: 2px solid hsl(300, 99%, 64%);
-  border-radius: 20px;
-  font-size: 16px;
-  color: #6c4539;
-  outline: none;
-  background-color: rgba(255, 255, 255, 0.5);
-  backdrop-filter: blur(5px);
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-
-  &::placeholder {
+    width: 30%;
+    padding: 10px;
+    border: 2px solid hsl(300, 99%, 64%);
+    border-radius: 20px;
+    font-size: 16px;
     color: #6c4539;
-  }
+    outline: none;
+    background-color: rgba(255, 255, 255, 0.5);
+    backdrop-filter: blur(5px);
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
+    &::placeholder {
+        color: #6c4539;
+    }
 `;
 
 const Menu = styled.div`
@@ -66,7 +66,6 @@ const MenuLink = styled.a`
   line-height: normal;
   margin-left: 15px;
   margin-right: 15px;
-
   &:hover {
     background-color: transparent;
     background-clip: text;
@@ -77,185 +76,92 @@ const MenuLink = styled.a`
   }
 `;
 
-const Secao = styled.section`
-  padding: 20px;
-  margin: 10px 0;
-`;
-
-
 const Icons = styled.div`
-  display: flex;
-  overflow: hidden;
-  justify-content: flex-end;
-  gap: 16px;
+    display: flex;
+    overflow: hidden;
+    justify-content: flex-end;
+    gap: 16px;
 `;
 
 const Perfil = styled.div`
-  display: flex;
-  align-items: center;
-  overflow: hidden;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
 `;
 
 const Noticacao = styled.div`
-  display: flex;
-  align-items: center;
-  overflow: hidden;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
 `;
 
 const Sair = styled.div`
-  display: flex;
-  align-items: center;
-  overflow: hidden;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
 `;
 
 const PerfilLink = styled.a`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 32px;
-  height: 32px;
-  cursor: pointer;
-  transition: transform 0.2s ease-in-out;
-  border: none;
-  background-color: transparent;
-  color: #6c4539;
-  overflow: hidden;
-  text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+    transition: transform 0.2s ease-in-out;
+    border: none;
+    background-color: transparent;
+    color: #6c4539;
+    overflow: hidden;
+    text-decoration: none;
 
-  &:hover {
-    transform: scale(1.1);
-    text-decoration: underline;
-    color: hsl(300, 99%, 64%);
-  }
+    &:hover {
+      transform: scale(1.1);
+      text-decoration: underline;
+      color: hsl(300, 99%, 64%);
+    }
 `;
 
 const NoticacaoLink = styled.a`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 32px;
-  height: 32px;
-  overflow: hidden;
-  cursor: pointer;
-  transition: transform 0.2s ease-in-out;
-  border: none;
-  background-color: transparent;
-  color: #6c4539;
-  text-decoration: none;
-
-  &:hover {
-    transform: scale(1.1);
-    text-decoration: underline;
-    color: hsl(300, 99%, 64%);
-  }
-`;
-
-const BotaoSair = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-  width: 32px;
-  height: 32px;
-  cursor: pointer;
-  transition: transform 0.2s ease-in-out;
-  border: none;
-  background-color: transparent;
-  color: #6c4539;
-
-  &:hover {
-    transform: scale(1.1);
-    text-decoration: underline;
-    color: hsl(300, 99%, 64%);
-  }
-`;
-
-const Title = styled.div`
-  text-align: left;
-  color: #6c4539;
-  font-style: bold;
-  list-style: none;
-  font-size: 40px;
-  margin-bottom: 20px;
-  margin-left: 50px;
-  margin-top: 20px;
-`;
-
-const Footer = styled.div`
-  background-color: #FFC9E9;
-  color: white;
-  padding: 40px 80px;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  border: 3px solid #6c4539;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  display: flex;
-`;
-
-const ConteudoFooter = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
-  justify-content: space-around;
-  width: 100%;
-  max-width: 1200px;
-`;
-
-const FooterLink = styled.a`
-  font-size: 15px;
-  color: #6c4539;
-  text-decoration: none;
-  font-weight: bold;
-  display: inline-block;
-  margin: 10px 0;
-  text-align: center;
-
-  &:hover {
-    background-color: transparent;
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: hsl(300, 99%, 64%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 32px;
+    height: 32px;
+    overflow: hidden;
     cursor: pointer;
-    transform: scale(1.1);
-  }
-`;
+    transition: transform 0.2s ease-in-out;
+    border: none;
+    background-color: transparent;
+    color: #6c4539;
+    text-decoration: none;
 
-const Coluna = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  flex: 1;
-  min-width: 200px;
+    &:hover {
+      transform: scale(1.1);
+      text-decoration: underline;
+      color: hsl(300, 99%, 64%);
+    }
 `;
-
-const SocialIcons = styled.div`
-  display: flex;
-  gap: 15px;
-  font-size: 22px;
-  width: 20px;
-  justify-content: center;
+  
+const BotaoSair = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+    transition: transform 0.2s ease-in-out;
+    border: none;
+    background-color: transparent;
+    color: #6c4539;
+    
+    &:hover {
+      transform: scale(1.1);
+      text-decoration: underline;
+      color: hsl(300, 99%, 64%);
+    }
 `;
-
-const Links = styled.div`
-  display: flex;
-  gap: 30px;
-  cursor: pointer;
-  color: #6c4539;
-  flex-wrap: wrap;
-  margin-top: 20px;
-`;
-
-const Direitos = styled.div`
-  width: 100%;
-  border-top: 1px solid #6c4539;
-  text-align: center;
-  padding-top: 10px;
-  margin-top: 20px;
-`;
-
 
 const DadosContainer = styled.div`
   display: flex;
@@ -264,6 +170,7 @@ const DadosContainer = styled.div`
   align-items: center;
   gap: 15px;
   padding: 20px;
+  white-space: pre-line; /* permite que \n funcione como quebra de linha */
 `;
 
 
@@ -284,22 +191,18 @@ const Opcoes = styled.div`
   }
 `;
 
-const TituloOpcoes = styled.div`
-  font-size: 30px;
-  font-weight: bold;
-`;
 
 
 const Descricao = styled.div`
-  font-size: 20px;
+  font-size: 20x;
   font-weight: normal;
   margin-top: 10px;
   display: ${(props) => (props.visivel ? "block" : "none")};
-  line-height: 1.5;
-  text-align: justify;
+  line-height: 1.5; /* Espaçamento entre as linhas */
+  text-align: justify; /* Justificação do texto */
 `;
 
- const Label = styled.label`
+const Label = styled.label`
   display: block;
   font-weight: bold;
   margin-top: 10px;
@@ -360,6 +263,133 @@ const BotaoExcluir = styled(BotaoEnviar)`
     background-color: #cc0000;
   }
 `;
+
+const MeusDados = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  padding: 20px;
+  white-space: pre-line;
+`;
+
+// Exemplo de como criar e exportar os componentes ausentes:
+const TituloOpcoes = styled.h2`
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const Footer = styled.div`
+  background-color: #FFC9E9;
+  display: flex;
+  color: white;
+  padding: 40px 80px;
+  display: flex; /*conteiner manipulavel*/
+  flex-wrap: wrap;
+  justify-content: space-between;
+  border: 3px solid #6c4539;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const ConteudoFooter = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: space-around;
+  width: 100%;
+  max-width: 1200px;
+`;
+
+const FooterLink = styled.a`
+  font-size: 15px;
+  color: #6c4539;
+  text-decoration: none;
+  font-weight: bold;
+  border: 1px #FFC9E9;
+  display: inline-block;
+  line-height: normal;
+  text-decoration: none;
+  margin: 10px 0;
+  text-align: center;
+  &:hover {
+    background-color: transparent;
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: hsl(300, 99%, 64%);
+    cursor: pointer;
+    transform: scale(1.1);
+  }
+`;
+
+const Coluna = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center; 
+  flex: 1;
+  min-width: 200px;
+`;
+
+const SocialIcons = styled.div`
+  display: flex;
+  gap: 15px;
+  font-size: 22px;
+  width: 20px;
+  justify-content: center;
+`;
+
+const Links = styled.div`
+  display: flex;
+  gap: 30px;
+  cursor: pointer;
+  color: #6c4539;
+  flex-wrap: wrap;
+  margin-top: 20px;
+`;
+
+const Direitos = styled.div`
+  width: 100%;
+  border-top: 1px solid #6c4539;
+  text-align: center;
+  padding-top: 10px;
+  margin-top: 20px;
+`;
+
+const Texto = styled.p`
+  font-size: 12px;
+  color: #6c4539;
+`;
+
+const Logo = styled.img`
+  width: 100px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+`;
+
+const TitleFooter = styled.h3`
+    text-align: center;
+    color: #6c4539;
+    font-style: bold;
+    list-style: none;
+    margin-bottom: 10px;
+    font-size: 40px;
+`;
+
+const Titulo = styled.div`
+  color: #6c4539;
+  font-style: bold;
+  list-style: none;
+  font-size: 40px;
+  margin-bottom: 20px;
+  margin-left: 50px;
+  margin-top: 20px;
+
+`;
+
 export {
   Background,
   Header,
@@ -375,32 +405,26 @@ export {
   Noticacao,
   Sair,
   Icons,
-  Secao,
+
+  Opcoes,
+  TituloOpcoes,
+  Descricao,
   Label,
   Input,
   TextArea,
+  DadosContainer,
   BotaoEnviar,
   BotaoExcluir,
-  Title,
+  Titulo,
+
   Footer,
   ConteudoFooter,
   Coluna,
   SocialIcons,
   FooterLink,
   Links,
-  Direitos,
   Logo,
+  Direitos,
   TitleFooter,
-  Texto,
-  Titulo,
-  Opcoes,
-  Descricao,
-  MeusDados,
-  DadosContainer,
-  TituloOpcoes,
-  Label,
-  Input,
-  TextArea,
-  BotaoEnviar,
-  BotaoExcluir
+  Texto
 };
