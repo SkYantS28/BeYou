4bf1a0140a494a954faf9b7d86d7940fb35c7a9d
Sky Crizosti <166828481+SkYantS28@users.noreply.github.com>
2025-04-23 23:26:34 -0300

    Update DadosPerfil.js

--- DadosPerfil.js
+++ DadosPerfil.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { User, Bell, LogOut } from "lucide-react";
+import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
 import {
   Background,
   Header,
   LogoContainer,
   ImgLogo,
-  Perfil,
-  PerfilLink,
+  SearchBar,
+  Menu,
+  MenuLink,
   BotaoSair,
-  Footer,
-  Direitos,
+  PerfilLink,
+  NoticacaoLink,
+  Perfil,
+  Noticacao,
+  Sair,
+  Icons,
+
   Opcoes,
   TituloOpcoes,
   Descricao,
@@ -19,15 +27,19 @@ import {
   DadosContainer,
   BotaoEnviar,
   BotaoExcluir,
-  SearchBar,
-  Menu,
-  MenuLink,
-  Icons,
-  Noticacao,
-  NoticacaoLink,
-  Sair,
+  Titulo,
+
+  Footer,
+  ConteudoFooter,
+  Coluna,
+  SocialIcons,
+  FooterLink,
+  Links,
+  Logo,
+  Direitos,
+  TitleFooter,
+  Texto
 } from "./components/dadosperfilestrutura";
-import { User, Bell, LogOut } from "lucide-react";
 
 // Seção: Dados Pessoais
 function DadosPessoais() {
@@ -175,53 +187,59 @@ function TelaMeusDados() {
 
 // Componente principal
 function MeuPerfil() {
-  const [menuVisible, setMenuVisible] = useState(false);
-  const navigate = useNavigate();
+  const handleLogout = () => {
+    navigate("/login"); 
+  };
 
-  const toggleMenu = () => setMenuVisible(!menuVisible);
+  // linkar paginas
+  const navigate = useNavigate(); 
+
+  // levar ate a pagina inicial
   const reloadPage = (event) => {
     event.preventDefault();
-    window.location.reload();
-  };
-  const handleLogout = () => {
-    navigate("/");
+    navigate('/PaginaInicial');
   };
 
   return (
     <Background>
       <Header>
         <LogoContainer>
-          <ImgLogo onClick={reloadPage} src="/img/new_logo.jpg" alt="Logo" />
+            <ImgLogo onClick={reloadPage} src="/img/new_logo.jpg" alt="Logo" />
         </LogoContainer>
 
         <Menu>
-          <MenuLink onClick={() => navigate("/PaginaInicial")}>Início</MenuLink>
-          <MenuLink onClick={() => navigate("/Carrinho")}>Meu Carrinho</MenuLink>
-          <MenuLink onClick={() => navigate("/Pesquisar")}>Pesquisar</MenuLink>
-          <MenuLink onClick={() => navigate("/Agendamentos")}>Agendamentos</MenuLink>
-          <MenuLink onClick={() => navigate("/Favoritos_perfil")}>Favoritos</MenuLink>
-        </Menu>
+            <MenuLink onClick={() => navigate("/PaginaInicial")}>Inicio</MenuLink>
 
+            <MenuLink onClick={() => navigate("/Carrinho")}>Meu Carrinho</MenuLink>
+
+            <MenuLink onClick={() => navigate("/Pesquisar")}>Pesquisar</MenuLink>
+
+            <MenuLink onClick={() => navigate("/Agendamentos")}>Agendamentos</MenuLink>
+
+            <MenuLink onClick={() => navigate("/FAvoritos_perfil")}>Favoritos</MenuLink>
+
+        </Menu>
+        
         <SearchBar type="text" placeholder="Busque por item ou loja" />
 
         <Icons>
-          <Perfil>
-            <PerfilLink onClick={() => navigate("/MeuPerfil")}>
-              <User className="icone" />
-            </PerfilLink>
-          </Perfil>
+            <Perfil>
+                <PerfilLink onClick={() => navigate("/MeuPerfil")}>
+                    <User className="icone" />
+                </PerfilLink>
+            </Perfil>
 
-          <Noticacao>
-            <NoticacaoLink onClick={() => navigate("/notificacoes_perfil")}>
-              <Bell className="icone" />
-            </NoticacaoLink>
-          </Noticacao>
+            <Noticacao>
+                <NoticacaoLink onClick={() => navigate("/notificacoes_perfil")}>
+                    <Bell className="icone" />
+                </NoticacaoLink>
+            </Noticacao>
 
-          <Sair>
-            <BotaoSair onClick={handleLogout}>
-              <LogOut className="icone" />
-            </BotaoSair>
-          </Sair>
+            <Sair>
+                <BotaoSair onClick={handleLogout}>
+                    <LogOut className="icone" />
+                </BotaoSair>
+            </Sair>
         </Icons>
       </Header>
 
@@ -230,9 +248,53 @@ function MeuPerfil() {
       </main>
 
       <Footer>
-        <Direitos>
-          <p>Todos os direitos reservados. ©2025 por BeYou</p>
-        </Direitos>
+        <ConteudoFooter>
+            <Coluna>
+                <TitleFooter>BeYou</TitleFooter>
+
+                <FooterLink>Site Institucional</FooterLink>
+                <FooterLink>Fale Conosco</FooterLink>
+                <FooterLink>Conta e Segurança</FooterLink>
+                <FooterLink>Carreiras</FooterLink>
+                <FooterLink>Entregadores</FooterLink>
+
+            </Coluna>
+
+            <Coluna>
+                <TitleFooter>Descubra</TitleFooter>
+
+                <FooterLink>Cadastre sua loja</FooterLink>
+                <FooterLink>Beyou Shop</FooterLink>
+                <FooterLink>BeYou Empresas</FooterLink>
+                <FooterLink>Blog BeYou Empresas</FooterLink>
+            </Coluna>
+            
+            <Coluna>
+                <TitleFooter>Social</TitleFooter>
+                <SocialIcons>
+                    <FooterLink><FaFacebookF/></FooterLink>
+                    <FooterLink><FaTwitter/></FooterLink>
+                    <FooterLink><FaYoutube/></FooterLink>
+                    <FooterLink><FaInstagram/></FooterLink>
+                </SocialIcons>
+            </Coluna>
+            
+            <Direitos>
+
+            <Logo src="/img/new_logo.jpg" alt="BeYou Logo" />
+            
+            <Texto>© Copyright 2025 - BeYou. Todos os direitos reservados.</Texto>
+
+            <br></br>
+            
+            <Texto>CNPJ 00.000.000/0000-00 / Endereço fictício - Cidade/Estado - CEP 00000-000</Texto>
+
+            </Direitos>
+
+            <Links>
+                <FooterLink>Termos e Condições de Uso</FooterLink> | <FooterLink>Codigo de Conduta</FooterLink> | <FooterLink>Privacidade</FooterLink> | <FooterLink>Dicas de Segurança</FooterLink>
+            </Links>
+        </ConteudoFooter>
       </Footer>
     </Background>
   );
